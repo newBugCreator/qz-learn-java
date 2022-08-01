000e7e9c3470f293ebfe9878fe28f09311aedbff
QinZhou <1185917912@qq.com>
2022-08-01 23:58:14 +0800

    fist commit.

---
+++ first-spring-boot-application/src/main/java/com/qz/springboot/App.java
@@ -0,0 +1,25 @@
+package com.qz.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@RestController
+@SpringBootApplication
+public class App 
+{
+    @RequestMapping("/")
+    public String index(){
+        return "Welcome";
+    }
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+        System.out.println( "Hello World!" );
+    }
+}
